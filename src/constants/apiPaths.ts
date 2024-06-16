cc5d744c636301c0c4444fe932b20b44cfaab010
russki007 <noreply@russki007.com>
2024-06-17 08:38:56 +1200

    task-3: updated to use product api endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://pirioe4xbg.execute-api.ap-southeast-2.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
